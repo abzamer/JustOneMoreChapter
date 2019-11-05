934fc82411a34e51d3eddc3d53797ece048f0ddd
Angelica Zamer <angelica.zamer@gmail.com>
2019-11-04 22:03:55 -0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import SearchForm from "./Components/SearchForm/SearchForm";
+import Button from "./Components/Button";
+import Navbar from "./Components/Nav";
+import Jumbotron from "./Components/Jumbotron";
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Jumbotron />
+      <SearchForm />
+      <Button />
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/Components/SearchForm/SearchForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function SearchForm(props) {
+  return (
+    <div className="input-group input-group-lg">
+      <input className="form-control" type="text" {...props} />
+    </div>
+  );
+}
+
+
+export default SearchForm;
