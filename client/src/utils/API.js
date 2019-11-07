c60180b46f787183500c8d6853ff47d48ee06884
Angelica Zamer <angelica.zamer@gmail.com>
2019-11-06 22:21:15 -0600

    trying out routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 
 export default {
   getBooks: function(query) {
-    var bookUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + query + '&maxResults=4&orderBy=relevance&key=' + process.env.GOOGLE_BOOKS_API_KEY
+
+    var bookUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + query + process.env.GOOGLE_BOOKS_API_KEY;
+    console.log(bookUrl);
     return axios.get(bookUrl);
   }
 };
