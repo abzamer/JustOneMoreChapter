4ee88375970c4f2edee04ace0615fa9496cca0b5
Angelica Zamer <angelica.zamer@gmail.com>
2019-11-06 21:40:13 -0600

    switched package.json files. somehow swapped

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,10 +3,9 @@
 import axios from "axios";
 require('dotenv').config();
 
-const GOOGLE_BOOKS_API_KEY = "AIzaSyDIbflXOLr0kGKCy4Y8Z2heJdvWZmYwGPY";
 export default {
   getBooks: function(query) {
-    var bookUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + query + '&maxResults=4&orderBy=relevance&key=' + GOOGLE_BOOKS_API_KEY
+    var bookUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + query + '&maxResults=4&orderBy=relevance&key=' + process.env.GOOGLE_BOOKS_API_KEY
     return axios.get(bookUrl);
   }
 };
